5d98fae7883ffc259b4e996952761054a4b63bba
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-05-29 00:44:21 -0500

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./Styles/index.scss";
-import { Footer } from "./Components/Molecules";
+import { Footer, Header } from "./Components/Molecules";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Footer />
     </div>
   );
