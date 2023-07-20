eb2169c59cd5841e0cb20315e832cb9a1a9e3ee4
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-07-20 13:32:30 -0500

    changes

--- src/Components/Molecules/Footer/Footer.tsx
+++ src/Components/Molecules/Footer/Footer.tsx
@@ -5,11 +5,13 @@ import { IMAGE_RULES } from "../../../Resources/Images/Images";
 
 export default function Footer() {
   const [enableModalRules, setEnableModalRules] = useState(false);
-  const Toggle = () => setEnableModalRules(!enableModalRules);
+  // TODO: revisar porque no se debe usar el valor del estado
+  const Toggle = (previusState) => setEnableModalRules(previusState);
+
   const rulesModalTille = 'RULES';
   return (
     <footer className='footer'>
-       <button className="footer-button" onClick={() => Toggle()}>
+       <button className="footer-button" onClick={() => Toggle(!enableModalRules)}>
         {rulesModalTille}
       </button>
       <ImageRender enableModal={enableModalRules} close={Toggle} img={IMAGE_RULES} title={rulesModalTille}/>
